45c851b4f863a487db0e30986833e3ab180257ec
Madhav Kumar <madhavampire@gmail.com>
2023-07-03 02:23:56 +0530

    final

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from os import system as cmd
 cmd("pip install -r requirements.txt")
 
 # start
-from threading import Thread
 from web_app import start_web_server
 
-Thread(target=start_web_server).start()
+start_web_server()
