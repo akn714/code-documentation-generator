e134bf6d01e916a047e93f13685121b21bd2bb55
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-03 00:33:47 +0530

    updated api.py

--- api.py
+++ api.py
@@ -1,32 +1,25 @@
-from pychatgpt import Chat, Options
-
-options = Options()
-
-# Enable, Disable logs
-options.log = True
-
-# Track conversation
-options.track = True
-
-# Use a proxy
-options.proxies = 'https://code-documentation-generator.adarshkumar35.repl.co'
-
-# Create a Chat object
-chat = Chat(email="email", password="password", options=options)
-
-
-# importing flask framework
 from flask import Flask, request
+from gpt4free import you
 
 app = Flask(__name__)
+
+def GPT_response(prompt, detailed=False, include_links=False):
+    return you.Completion.create(
+        prompt=prompt,
+        detailed=detailed,
+        include_links=include_links
+    )
+
+with open("query_prompt.txt", "r") as file:
+    prompt = file.read()
 
 # api endpoint
 @app.route('/api/generate', methods=['POST'])
 def docGen():
     code = request.form['code']
-    print(code)
-    res = chat.ask(f"please generate a documentation in markdown formate for the code {code}")
-    return res
+    query = prompt+code
+    response = GPT_response(query).text;
+    return response
 
 # running the server
 app.run(host='0.0.0.0', port='8080', debug=True)
