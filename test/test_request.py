8699fe1c49a2329b7e03ee5d8cd8342aa4f54f99
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-03 03:09:15 +0530

    added api link

--- test/test_request.py
+++ test/test_request.py
@@ -1,13 +1,13 @@
 import requests
 
 r = requests.post(
-    "http://10.184.23.197:8080/api/generate",
-    data = {
-        "code": """
+    "https://code-documentation-generator.adarshkumar35.repl.co/api/generate",
+    data={
+        "code":
+        """
             def add(a,b):
                 return a+B
         """
-    }
-)
+    })
 
 print(r.text)  # displays the result body.
