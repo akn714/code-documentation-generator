a1a78b98a1a9743d039d988788e5d7f9deb4b538
Adarsh Kumar <123639529+adarshkumar714@users.noreply.github.com>
2023-07-03 00:43:34 +0530

    created test_request.py

---
+++ test_request.py
@@ -0,0 +1,12 @@
+import requests
+
+r = requests.post(
+    "https://code-documentation-generator.adarshkumar35.repl.co/api/generate",
+    data={'code': 
+"""
+def add(a, b):
+	return a + b
+"""
+})
+# And done.
+print(r.text)  # displays the result body.
